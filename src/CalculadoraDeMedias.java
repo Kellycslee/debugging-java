2aac8ac14bc43e4af36eba31f08971cd5928a12b
Kellycslee <souza.kelly@hotmail.com>
2023-04-10 12:26:51 -0300

    teste inicial

---
+++ src/CalculadoraDeMedias.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CalculadoraDeMedias {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Camila", "Lucas", "Bruna", "Pedro"};
+
+        // se colocar variavel int media para calcular a media aparece apenas
+        // numeros inteiros e não quebrados, para incluir sempre utilizar double
+
+        double media = calculaMediaDaTurma(alunos, scan);
+
+        System.out.printf("Média da turma %.1f", media);
+    }
+
+    public static double calculaMediaDaTurma(String[] alunos, Scanner scanner) {
+
+        double soma = 0;
+        for(String aluno : alunos) {
+            System.out.printf("Nota do aluno %s: ", aluno);
+            double nota = scanner.nextDouble();
+            soma += nota;
+        }
+
+        return soma / alunos.length;
+    }
+
+}
